834cfefa4fd72a43abca56e1f7563bbfbb20b4a6
tejendra6574 <tejendrag@gmail.com>
2023-04-06 14:53:17 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3')
+streamlit.text('Omlette')
+streamlit.text('Boiled Egg')
